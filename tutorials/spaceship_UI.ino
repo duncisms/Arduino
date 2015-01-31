2ca8f4efb5408599baca8c4562c13e427f97d751
duncisms <dphyfe@gmail.com>
2015-01-30 23:57:33 -0500

    Arduino Tutorials

---
+++ tutorials/spaceship_UI.ino
@@ -0,0 +1,31 @@
+int switchState = 0;
+
+void setup() {
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(2);
+  // Yes my first coding expeirence
+  
+  if (switchState == LOW) { // the button is not pressed
+   digitalWrite(3, HIGH); //green LED
+   digitalWrite(4, LOW); //red LED
+   digitalWrite(5, LOW); //red LED
+ }
+  else { //the button is pressed
+   digitalWrite(3, LOW);
+   digitalWrite(4, LOW);
+   digitalWrite(5, HIGH);
+   
+   delay(250); //wait for a quater second
+   // toggle the LEDs
+   digitalWrite(4, HIGH);
+   digitalWrite(5, LOW);
+   delay(250); //wait for a quater second
+   }
+} // go back to the beginning of the loop
+
